04b8673fd04f96e320b8b676405abd6144aa2399
Jenny <tuluber@gmail.com>
2025-04-27 12:44:07 +0800

    優化java mail

--- src/main/java/com/ntou/sysintegrat/mailserver/JavaMail.java
+++ src/main/java/com/ntou/sysintegrat/mailserver/JavaMail.java
@@ -1,5 +1,6 @@
 package com.ntou.sysintegrat.mailserver;
 
+import com.ntou.tool.Common;
 import lombok.extern.log4j.Log4j2;
 
 import jakarta.mail.Authenticator;
@@ -18,6 +19,15 @@ import java.util.concurrent.Executors;
 @Log4j2
 public class JavaMail {
     private static final ExecutorService executor = Executors.newFixedThreadPool(5);
+    private static final String userName = "weiiweiidev@gmail.com"; // 寄件者信箱
+    private static final String password = "gevaewtbwiwhedea"; // 寄件者密碼
+    private static final Properties prop = getProperties();
+    private static final Session session = Session.getInstance(prop, new Authenticator() {
+        @Override
+        protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(userName, password);
+        }
+    });
 
     static {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
@@ -28,29 +38,41 @@ public class JavaMail {
 
     public void sendMail(MailVO vo) {
         executor.submit(() -> {
-            final String userName = "weiiweiidev@gmail.com";
-            final String password = "gevaewtbwiwhedea";
-
-            Properties prop = getProperties();
-            Session session = Session.getDefaultInstance(prop, new Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(userName, password);
-                }
-            });
-
+            Transport transport = null;
             try {
                 MimeMessage message = new MimeMessage(session);
-                message.setFrom(new InternetAddress(userName));
-                message.setRecipient(RecipientType.TO, new InternetAddress(vo.getEmailAddr()));
-                message.setSubject(vo.getSubject());
-                message.setContent(vo.getContent(), "text/html;charset=UTF-8");
 
-                Transport.send(message);
-                log.info("寄信成功：{}", vo.getEmailAddr());
+                try {
+                    InternetAddress sender = new InternetAddress(userName);
+                    message.setSender(sender);
 
-            } catch (MessagingException e) {
-                log.error("寄信失敗", e);
+                    // 收件者
+                    message.setRecipient(RecipientType.TO, new InternetAddress(vo.getEmailAddr()));
+
+                    // 標題
+                    message.setSubject(vo.getSubject());
+
+                    // 內容/格式
+                    message.setContent(vo.getContent(), "text/html;charset = UTF-8");
+
+                    // ---------------------------------------------------------Transport傳送Message
+                    transport = session.getTransport();
+                    transport.connect(userName, password); // 顯式連接
+
+                    // transport將message送出
+                    transport.sendMessage(message, message.getAllRecipients());
+
+                } catch (MessagingException e) {
+                    log.error(Common.EXCEPTION, e);
+                } finally {
+                    try {
+                        if (transport != null && transport.isConnected()) {
+                            transport.close();
+                        }
+                    } catch (Exception e) {
+                        log.error(Common.EXCEPTION, e);
+                    }
+                }
             } catch (Exception e) {
                 log.error("非同步寄信失敗", e);
             }
@@ -59,13 +81,27 @@ public class JavaMail {
 
     private static Properties getProperties() {
         Properties prop = new Properties();
+
+        // 設定連線為smtp
         prop.setProperty("mail.transport.protocol", "smtp");
+
+        // host主機:smtp.gmail.com
         prop.setProperty("mail.host", "smtp.gmail.com");
-        prop.setProperty("mail.smtp.port", "465");
-        prop.setProperty("mail.smtp.auth", "true");
-        prop.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        prop.setProperty("mail.smtp.socketFactory.port", "465");
-        prop.setProperty("mail.debug", "true");
+
+        // host port:465
+        prop.put("mail.smtp.port", "465");
+
+        // 寄件者帳號需要驗證：是
+        prop.put("mail.smtp.auth", "true");
+
+        // 需要安全資料傳輸層 (SSL)：是
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+        // 安全資料傳輸層 (SSL) 通訊埠：465
+        prop.put("mail.smtp.socketFactory.port", "465");
+
+        // 顯示連線資訊 (debug 模式，可以先開啟方便排查問題)
+        prop.put("mail.debug", "true");
         return prop;
     }
 }
